ff4b3e2ab43bf8edfddb53bd48d40dacc6f25c0c
anisnuhic <anis.nuhic30@gmail.com>
2024-08-13 16:07:01 +0200

    dial

--- go.mod
+++ go.mod
@@ -3,18 +3,7 @@ module ARI
 go 1.22.5
 
 require (
-	github.com/CyCoreSystems/ari/v6 v6.0.0-20231212042517-0e2dabc3bcb1 // indirect
-	github.com/apcera/nats v1.7.2 // indirect
-	github.com/gogo/protobuf v1.3.2 // indirect
-	github.com/nats-io/go-nats v1.7.2 // indirect
-	github.com/nats-io/nkeys v0.4.7 // indirect
-	github.com/nats-io/nuid v1.0.1 // indirect
-	github.com/nvisibleinc/go-ari-library v0.0.0-20150318174539-8d54a15dc2a4 // indirect
-	github.com/oklog/ulid v1.3.1 // indirect
-	github.com/rotisserie/eris v0.5.4 // indirect
-	github.com/streadway/amqp v1.1.0 // indirect
-	golang.org/x/crypto v0.25.0 // indirect
-	golang.org/x/exp v0.0.0-20230905200255-921286631fa9 // indirect
-	golang.org/x/net v0.21.0 // indirect
-	golang.org/x/sys v0.22.0 // indirect
+	github.com/abourget/ari v0.0.0-20170827223515-e20fb2863cbe // indirect
+	github.com/jmcvetta/napping v3.2.0+incompatible // indirect
+	golang.org/x/net v0.28.0 // indirect
 )
--- main.go
+++ main.go
@@ -1,92 +1,138 @@
 package main
 
 import (
-	//"context"
+	"bufio"
+	//"flag"
 	"fmt"
-	"log"
+	"os"
 	"strings"
-	"time"
 
-	"github.com/CyCoreSystems/ari/v6"
-	"github.com/CyCoreSystems/ari/v6/client/native"
+	"github.com/abourget/ari"
 )
 
-var ariClient ari.Client
-
 func main() {
-	_, err := native.Connect(&native.Options{
-		Application: "main",
-		Username:    "asterisk",
-		Password:    "pass",
-		URL:         "http://localhost:8088/ari",
-	})
-	if err != nil {
-		log.Fatalf("Error connecting to server ARI: %v", err)
-	}
+	// Kreiramo klijenta
+	username := "main"
+	password := "pass"
+	hostname := "localhost"
+	port := 8088
+	appName := "my_app"
+	client := ari.NewClient(username, password, hostname, port, appName)
 
-	fmt.Println("Connected to ARI server")
-}
-func handleDial(args []string) {
-	if len(args) < 2 {
-		fmt.Println("Usage: dial <endpoint1> <endpoint2> <endpoint3> ...")
-		return
-	}
+	// Pokrećemo listener za događaje
+	eventsChannel := client.LaunchListener()
 
-	callId := fmt.Sprintf("call-%d", time.Now().UnixNano())
-	endpoints := args
+	// Pratimo događaje iz kanala u zasebnoj gorutini
+	go func() {
+		for event := range eventsChannel {
+			fmt.Printf("Primljen događaj: %v\n", event)
+		}
+	}()
 
-	for i := 0; i < len(endpoints); i++ {
-		source := endpoints[i]
-		destination := endpoints[i+i]
-		err := originateCall(source, destination, callId)
-		if err != nil {
-			fmt.Println("Error initiating call:", err)
-			return
+	fmt.Println("Unesite komande (dial <from> <to>, list, join <bridge_id>):")
+	scanner := bufio.NewScanner(os.Stdin)
+
+	for {
+		// Čekamo korisnički unos
+		scanner.Scan()
+		input := scanner.Text()
+
+		// Parsiramo unos
+		parts := strings.Fields(input)
+		if len(parts) == 0 {
+			continue
+		}
+
+		switch parts[0] {
+		case "dial":
+			if len(parts) != 3 {
+				fmt.Println("Neispravan unos za dial. Koristite: dial <from> <to>")
+				continue
+			}
+			from := parts[1]
+			to := parts[2]
+			err := Dial(client, from, to)
+			if err != nil {
+				fmt.Println("Greška pri pokretanju poziva:", err)
+			}
+		case "list":
+			err := List(client)
+			if err != nil {
+				fmt.Println("Greška pri listanju poziva:", err)
+			}
+		case "join":
+			if len(parts) != 2 {
+				fmt.Println("Neispravan unos za join. Koristite: join <bridge_id>")
+				continue
+			}
+			bridgeID := parts[1]
+			err := Join(client, bridgeID)
+			if err != nil {
+				fmt.Println("Greška pri pridruživanju pozivu:", err)
+			}
+		default:
+			fmt.Println("Nepoznata komanda. Koristite: dial <from> <to>, list, join <bridge_id>")
 		}
 	}
-	fmt.Println("Call initiated with ID: ", callId)
 }
 
-func originateCall(source, destination, callId string) error {
-	_, err := ariClient.Channel().Create(nil, ari.ChannelCreateRequest{
-		Endpoint:  source,
-		App:       "main",
-		ChannelID: callId,
-	})
-	if err != nil {
-		return fmt.Errorf("Error creating channel for call %s: %v", source, err)
+// Dial pokreće novi poziv između dve ekstenzije
+func Dial(client *ari.Client, from, to string) error {
+	// Kreiramo parametre za Originate (pozivanje)
+	paramsFrom := ari.OriginateParams{
+		Endpoint:  "PJSIP/" + from, // Pretpostavljam da koristiš SIP, promijeni prema potrebi
+		Extension: to,
+		Context:   "sets", // Postavi svoj kontekst
+		Priority:  1,
+		CallerID:  to, // Postavi svoj CallerID
+		Timeout:   30,
+	}
 
+	paramsTo := ari.OriginateParams{
+		Endpoint:  "PJSIP/" + to,
+		Extension: from,
+		Context:   "sets",
+		Priority:  1,
+		CallerID:  from,
+		Timeout:   30,
 	}
-	_, derr := ariClient.Channel().Originate(nil, ari.OriginateRequest{
-		Endpoint:  destination,
-		App:       "main",
-		ChannelID: callId,
-	})
-	if derr != nil {
-		return fmt.Errorf("Error originating call to destination %s %v", destination, err)
+
+	// Iniciramo novi kanal (poziv) između dve ekstenzije
+	_, err := client.Channels.Create(paramsFrom)
+	if err != nil {
+		return fmt.Errorf("neuspješno pokretanje poziva sa ekstenzije %s ka %s: %v", from, to, err)
 	}
+
+	_, err = client.Channels.Create(paramsTo)
+	if err != nil {
+		return fmt.Errorf("neuspješno pokretanje poziva sa ekstenzije %s ka %s: %v", to, from, err)
+	}
+
+	fmt.Printf("Poziv pokrenut između ekstenzija: %s i %s\n", from, to)
 	return nil
 }
-func handleList() {
-	//implement this function
-}
-func handleJoin(args []string) {
-	//implement this function
-}
 
-func chooseOption(input string) {
-	parts := strings.Split(input, " ")
-	command := parts[0]
-	args := parts[1:]
-
-	switch command {
-	case "dial":
-		handleDial(args)
-	case "list":
-		handleList()
-	case "join":
-		handleJoin(args)
-	default:
-		fmt.Println("Invalid command. Use 'dial', 'list', or 'join' followed by phone number.")
+// List prikazuje trenutne pozive
+// List prikazuje trenutne pozive
+func List(client *ari.Client) error {
+	// Dohvatimo sve kanale
+	channels, err := client.Channels.List()
+	if err != nil {
+		return fmt.Errorf("neuspješno listanje kanala: %v", err)
 	}
+
+	fmt.Println("Lista trenutnih poziva:")
+	for _, channel := range channels {
+		fmt.Printf("Kanal ID: %s, Endpoint: %s, Status: %s\n", channel.ID, channel.Connected, channel.State)
+	}
+
+	return nil
+}
+
+
+// Join pridružuje korisnika mostu poziva
+func Join(client *ari.Client, bridgeID string) error {
+	// Ovdje bi koristili Bridges service za pridruživanje pozivu
+	fmt.Println("Pridruživanje mostu:", bridgeID)
+	return nil
 }
